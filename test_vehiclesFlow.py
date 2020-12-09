a4a96a156692abad6e0091b042f971759cc5c711
ccordob <catalina.cordoba@morningstar.com>
2020-12-08 20:58:37 -0500

    vehicle test version 1

--- test_vehiclesFlow.py
+++ test_vehiclesFlow.py
@@ -16,7 +16,7 @@ class VehiclesFlow(unittest.TestCase):
         "platformName": "Android",
         "automationName": "UiAutomator2",
         "deviceName": "emulator-5556",
-        "app": "/Users/ccordob/Downloads/"
+        "app": "/Users/ccordob/Downloads/com.evancharlton.mileage.apk"
     }
     testName = 'Untitled'
     driver = None
@@ -32,11 +32,51 @@ class VehiclesFlow(unittest.TestCase):
         cls.dc['platformName'] = 'android'
         cls.driver = webdriver.Remote('http://localhost:4723/wd/hub', cls.dc)
 
-    def test_edit_vehicle(self):
+    def test_create_vehicle_with_correct_values(self):
         self.driver.find_element_by_id('com.android.permissioncontroller:id/continue_button').click()
         time.sleep(1)
         self.driver.find_element_by_id('android:id/button1').click()
-        time.sleep(2)
+        time.sleep(1)
+        #self.driver.find_element_by_xpath(
+        #    "xpath=//[@id='icon' and (./preceding-sibling:: | ./following-sibling::*)[@text='Vehicles']]").click()
+        time.sleep(1)
+        self.driver.find_element_by_id("android:id/text1").click()
+        time.sleep(1)
+        self.driver.find_element_by_id('com.evancharlton.mileage:id/title').send_keys('TestVehicleName')
+        self.driver.find_element_by_id('com.evancharlton.mileage:id/year').send_keys('2020')
+        self.driver.find_element_by_id('com.evancharlton.mileage:id/make').send_keys('Test-Make')
+        self.driver.find_element_by_id('com.evancharlton.mileage:id/model').send_keys('Test-Model')
+        self.driver.find_element_by_id('com.evancharlton.mileage:id/description').send_keys('Test-Description')
+        self.driver.scroll(self.driver.find_element_by_id("com.evancharlton.mileage:id/type"),
+                           self.driver.find_element_by_id("com.evancharlton.mileage:id/title"))
+        self.driver.find_element_by_id('com.evancharlton.mileage:id/make_default').click()
+        self.driver.find_element_by_id('com.evancharlton.mileage:id/distance').click()
+        time.sleep(1)
+        self.driver.find_element_by_xpath("//*[@text='Kilometers']").click()
+        time.sleep(1)
+        self.driver.scroll(self.driver.find_element_by_id('com.evancharlton.mileage:id/distance'),
+                           self.driver.find_element_by_id("com.evancharlton.mileage:id/description"))
+        self.driver.find_element_by_id('com.evancharlton.mileage:id/volume').click()
+        time.sleep(1)
+        self.driver.find_element_by_xpath("//*[@text='Litres']").click()
+        self.driver.find_element_by_id('com.evancharlton.mileage:id/economy').click()
+        time.sleep(1)
+        self.driver.find_element_by_xpath("//*[@text='Km / Litre']").click()
+        self.driver.find_element_by_id('com.evancharlton.mileage:id/currency').send_keys('$')
+        self.driver.find_element_by_id('com.evancharlton.mileage:id/save_btn').click()
+        time.sleep(1)
+        name = self.driver.find_element_by_id("android:id/text1").get_attribute(
+            "text")
+        self.assertEqual(name, 'TestVehicleName')
+
+    def test_edit_vehicle_with_correct_values(self):
+        self.driver.find_element_by_id("android:id/text1").click()
+        time.sleep(1)
+        #TODO validate dta in form and edit with new values
+
+    def test_edit_vehicle_with_incorrect_values(self):
+        self.driver.find_element_by_id("android:id/text1").click()
+        #TODO
 
     @classmethod
     def tearDownClass(cls):
