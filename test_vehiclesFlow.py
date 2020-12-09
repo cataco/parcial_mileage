ba85df4f8aa4fafd7e4bed448e7bc68f8dd3ef27
ccordob <catalina.cordoba@morningstar.com>
2020-12-09 07:30:03 -0500

    update vehicle tests

--- test_vehiclesFlow.py
+++ test_vehiclesFlow.py
@@ -37,7 +37,7 @@ class VehiclesFlow(unittest.TestCase):
         time.sleep(1)
         self.driver.find_element_by_id('android:id/button1').click()
         time.sleep(1)
-        #self.driver.find_element_by_xpath(
+        # self.driver.find_element_by_xpath(
         #    "xpath=//[@id='icon' and (./preceding-sibling:: | ./following-sibling::*)[@text='Vehicles']]").click()
         time.sleep(1)
         self.driver.find_element_by_id("android:id/text1").click()
@@ -72,11 +72,82 @@ class VehiclesFlow(unittest.TestCase):
     def test_edit_vehicle_with_correct_values(self):
         self.driver.find_element_by_id("android:id/text1").click()
         time.sleep(1)
-        #TODO validate dta in form and edit with new values
+        self.driver.find_element_by_id('com.evancharlton.mileage:id/title').send_keys('-EDIT')
+        self.driver.find_element_by_id('com.evancharlton.mileage:id/make').send_keys('-EDIT')
+        self.driver.find_element_by_id('com.evancharlton.mileage:id/model').send_keys('-EDIT')
+        self.driver.find_element_by_id('com.evancharlton.mileage:id/description').send_keys('-EDIT')
+        self.driver.find_element_by_id('com.evancharlton.mileage:id/save_btn').click()
+        time.sleep(1)
+        title = self.driver.find_element_by_id("android:id/text1").get_attribute(
+            "text")
+        self.assertEqual(title, '-EDIT')
+        self.driver.find_element_by_id("android:id/text1").click()
+        time.sleep(1)
+        title = self.driver.find_element_by_id('com.evancharlton.mileage:id/title').get_attribute(
+            "text")
+        self.assertEqual(title, '-EDIT')
+        year = self.driver.find_element_by_id('com.evancharlton.mileage:id/year').get_attribute(
+            "text")
+        self.assertEqual(year, '2020')
+        make = self.driver.find_element_by_id('com.evancharlton.mileage:id/make').get_attribute(
+            "text")
+        self.assertEqual(make, '-EDIT')
+        model = self.driver.find_element_by_id('com.evancharlton.mileage:id/model').get_attribute(
+            "text")
+        self.assertEqual(model, '-EDIT')
+        currency = self.driver.find_element_by_id('com.evancharlton.mileage:id/currency').get_attribute(
+            "text")
+        self.assertEqual(currency, '$$')
+        self.driver.back()
 
     def test_edit_vehicle_with_incorrect_values(self):
         self.driver.find_element_by_id("android:id/text1").click()
-        #TODO
+        time.sleep(1)
+        self.driver.find_element_by_id('com.evancharlton.mileage:id/title').clear()
+        self.driver.find_element_by_id('com.evancharlton.mileage:id/year').clear().send_keys('abcd')
+        time.sleep(1)
+        self.driver.find_element_by_id('com.evancharlton.mileage:id/save_btn').click()
+        time.sleep(1)
+        self.assertTrue(self.driver.find_element_by_xpath("//*[@text='Invalid vehicle title']").is_displayed())
+        year = self.driver.find_element_by_id('com.evancharlton.mileage:id/year').get_attribute("text")
+        self.assertEqual(year, 'Year')
+
+    def test_validate_statistics_metrics_match_vehicle(self):
+        # Update values in Car
+        self.driver.find_element_by_id("android:id/text1").click()
+        time.sleep(1)
+        self.driver.find_element_by_id('com.evancharlton.mileage:id/distance').click()
+        time.sleep(1)
+        self.driver.find_element_by_xpath("//*[@text='Kilometers']").click()
+        self.driver.find_element_by_id('com.evancharlton.mileage:id/economy').click()
+        time.sleep(1)
+        self.driver.find_element_by_xpath("//*[@text='Km / Gallon']").click()
+        self.driver.find_element_by_id('com.evancharlton.mileage:id/save_btn').click()
+        time.sleep(1)
+        # Add fill upp
+        # self.driver.find_element_by_xpath(
+        #    "xpath=//[@id='icon' and (./preceding-sibling:: | ./following-sibling::*)[@text='Fillup']]").click()
+        time.sleep(1)
+        self.driver.find_element_by_id('com.evancharlton.mileage:id/price').send_keys('5')
+        self.driver.find_element_by_id('com.evancharlton.mileage:id/volume').send_keys('10')
+        self.driver.find_element_by_id('com.evancharlton.mileage:id/odometer').send_keys('100')
+        self.driver.find_element_by_id('com.evancharlton.mileage:id/save_btn').click()
+        time.sleep(1)
+        # self.driver.find_element_by_xpath(
+        #    "xpath=//[@id='icon' and (./preceding-sibling:: | ./following-sibling::*)[@text='Fillup']]").click()
+        time.sleep(1)
+        self.driver.find_element_by_id('com.evancharlton.mileage:id/price').send_keys('5')
+        self.driver.find_element_by_id('com.evancharlton.mileage:id/volume').send_keys('10')
+        self.driver.find_element_by_id('com.evancharlton.mileage:id/odometer').send_keys('200')
+        self.driver.find_element_by_id('com.evancharlton.mileage:id/save_btn').click()
+        time.sleep(1)
+        # self.driver.find_element_by_xpath(
+        #    "xpath=//[@id='icon' and (./preceding-sibling:: | ./following-sibling::*)[@text='Statistics']]").click()
+        self.assertEqual(self.driver.find_element_by_xpath("//*[@text='10.00 km/g']").size(), 3) #fuel economy
+        self.assertEqual(self.driver.find_element_by_xpath("//*[@text='$$100.00']").size(), 1) #total cost
+        self.assertEqual(self.driver.find_element_by_xpath("//*[@text='10.00 Gallons']").size(), 3) #fuel consumption
+
+
 
     @classmethod
     def tearDownClass(cls):
